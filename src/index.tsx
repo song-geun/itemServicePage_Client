23ee67ef2a3b1495b6cc7b37e3fa493b018210c6
brownmole01 <brownmole01@naver.com>
2024-08-15 13:02:39 +0900

    0815

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { store } from './API/store';
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
+    </BrowserRouter>
   </Provider>
 );
 
