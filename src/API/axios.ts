25436c1bfa490d328020170847ca9fc0a4cfe091
brown <brownmole-1@naver.com>
2024-08-18 09:43:56 +0900

    complete

--- src/API/axios.ts
+++ src/API/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 export const instance : any = 
     axios.create({
-    baseURL : 'http://127.0.0.1:8080/',
+    baseURL : 'http://192.168.219.100:8080/',
     headers : {
         accept: 'application/json',"Content-Type": `application/json`,
     }});
